fb8866538940ac024f313dd28700df9373276bed
beebeambap <rana6655544@gmail.com>
2026-05-02 18:23:24 +0900

    Update Supabase URL and anon key in config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 //
 // anon key는 Supabase가 의도한 공개 키이며, RLS 정책으로 보호되므로
 // 이 파일을 git에 그대로 커밋해도 안전합니다.
-window.SUPABASE_URL = '';
-window.SUPABASE_ANON_KEY = '';
+window.SUPABASE_URL = 'https://supabase.com/dashboard/project/wwdsmerrdtniavvzpkqs';
+window.SUPABASE_ANON_KEY = 'sb_publishable_93y0D0pVurt5Qpxhjdphtw_qogCoB5v';
